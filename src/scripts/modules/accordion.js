d8aa0ad5b0bf22977b8b7bc65de835677ea58a59
niksedov24 <nik.sedov890@gmail.com>
2024-10-22 09:56:03 +0400

    first commit

---
+++ src/scripts/modules/accordion.js
@@ -0,0 +1,9 @@
+export function accordion() {
+    
+}
+
+// Для корректной работы необходимо подключить и активировать эту функцию в app.js
+
+// Пример подключения: import { accordion } from "./путь/к/файлу/accordion.js";
+
+// Активация: accordion();
