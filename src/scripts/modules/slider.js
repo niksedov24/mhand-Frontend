b3c6619960e931f8053d20d3d6f66cbf6520c59b
niksedov24 <nik.sedov890@gmail.com>
2024-11-12 10:00:20 +0400

    upd

--- src/scripts/modules/slider.js
+++ src/scripts/modules/slider.js
@@ -14,3 +14,14 @@ new Swiper(".bannSlider", {
     prevEl: ".advantages-section__info-block__bann .button.prev",
   },
 });
+
+new Swiper(".brandsSlider", {
+  slidesPerView: 5,
+  spaceBetween: 21,
+  loop:true,
+  navigation: {
+    nextEl: ".brands-section__slider .button.next",
+    prevEl: ".brands-section__slider .button.prev",
+  },
+});
+
