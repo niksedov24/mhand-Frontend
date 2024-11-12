a525c212e40007f6ba61cd25234a44c3351502a8
niksedov24 <nik.sedov890@gmail.com>
2024-11-12 13:55:14 +0400

    upd

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,8 @@
 import { accordion } from './modules/accordion.js';
-import {slider} from  './modules/slider.js'
+import {slider} from  './modules/slider.js';
+import {utils} from './modules/utils.js';
 accordion()
 slider()
+utils()
+
+
---
+++ src/scripts/modules/utils.js
@@ -0,0 +1,6 @@
+export function utils() {
+    let burger = document.querySelector('.header-section__info-block__item__burger-logo__burger');
+    burger.addEventListener('click', function(event){
+        
+    })
+}
