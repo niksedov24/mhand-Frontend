a91dc4047ffb6adf0ef1bc8ae32461f7f19815fb
niksedov24 <nik.sedov890@gmail.com>
2024-12-15 13:56:03 +0300

    upd ( full adapt)

--- src/scripts/modules/utils.js
+++ src/scripts/modules/utils.js
@@ -3,6 +3,7 @@ export function utils() {
     let body = document.querySelector('body');
     let header = document.querySelector('header');
     let insert = document.createElement('div');
+    const footerContainers = document.querySelectorAll('.footer-section__info-block__menu__item');
     insert.className = "insert";
     body.prepend(insert)
     burger.addEventListener('click', function(event){
@@ -19,4 +20,14 @@ export function utils() {
         event.target.classList.remove('active');
         
     })
+    footerContainers.forEach(container => {
+        const button = container.querySelector('.footer-section__info-block__menu__item__title');
+        const list = container.querySelector('.footer__ul');
+     
+        button.addEventListener('click', function() {
+            list.classList.toggle('open');
+            button.classList.toggle('active');
+        });
+     });
+   
 }
